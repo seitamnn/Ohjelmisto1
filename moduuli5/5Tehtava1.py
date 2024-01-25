d665f6e64c6cf9753dd4c0649c644cc594597917
OMISTAJA <sofia.jarvela@metropolia.fi>
2024-01-25 13:02:43 +0200

    5.1.

---
+++ moduuli5/5Tehtava1.py
@@ -0,0 +1,11 @@
+# Kirjoita ohjelma, joka kysyy käyttäjältä arpakuutioiden lukumäärän.
+# Ohjelma heittää kerran kaikkia arpakuutioita ja tulostaa silmälukujen summan.
+# Käytä for-toistorakennetta.
+import random
+
+lukumaara = int(input("Anna arpakuutioiden lukumäärä: "))
+summa = 0
+for i in range(lukumaara):
+    nopat = random.randint(1,6)
+    summa += nopat
+print(f"Arpakuution silmälukujen summa on {summa}.")
